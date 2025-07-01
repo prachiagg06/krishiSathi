0a3e6089d9def48e2ffaa3c5ffd2b19fc1da99a7
devash0899 <devashsingh0899@gmail.com>
2025-07-01 22:52:32 +0530

    lauren

--- Frontend/Krishi-Sathi/src/pages/Home.jsx
+++ Frontend/Krishi-Sathi/src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import "../App.css"
-function Home(){
-  return(
+import "../App.css";
+function Home() {
+  return (
     <>
-    <div className="div main_content"><h1>HELLO DEVANSH</h1></div>
+      <div className="div main_content">
+        <h1>HELLO DEVANSH</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+          tenetur? Blanditiis, ratione quibusdam reiciendis officia omnis fugit
+          quae consectetur commodi impedit cumque corrupti molestiae unde nobis
+          est sapiente dolorem laudantium molestias provident fuga beatae ipsum
+          veritatis magni atque! Quas est id vero nesciunt quasi omnis, enim
+          harum in earum rerum possimus commodi corporis ea molestias deserunt
+          tenetur aliquam eum neque, velit cupiditate unde obcaecati voluptas
+          quisquam. Itaque sint quisquam unde enim similique hic accusantium
+          nemo fugiat, odit ratione iste odio. Maiores voluptatum dolorem natus
+          dignissimos laboriosam quisquam magnam debitis temporibus esse, et,
+          accusantium qui sapiente quae voluptas vero totam reiciendis.
+        </p>
+      </div>
     </>
-  )
+  );
 }
 export default Home;
